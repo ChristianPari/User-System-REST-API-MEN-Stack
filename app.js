f82e683da6d74d2f1e096ee50ac414773e07e424
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-08 14:35:47 -0500

    send message to user on '4000' using GET method

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const express = require('express'),
-      app = express();
+const express = require('express');
+const app = express();
+
+      app.get('/', (req,res) => {
+          app.send('hello user');
+      })
 
 app.listen('4000');
