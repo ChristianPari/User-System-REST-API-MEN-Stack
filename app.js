f55023391edfa6a9828eb729d2896ab8d22110b3
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-09 15:07:27 -0500

    added a middleware function and mongoose
    mongoose will connect a cloud server provided by mongodb

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const express = require('express');
+const express = require('express'),
+      app = express(),
+      mongoose = require('mongoose');
 
-const app = express();
+//Middle Ware
+
+app.use('/post', (req, res) => {
+    console.log('the post route was used');
+    
+})
 
 
 //Routes
@@ -10,6 +17,13 @@ app.get('/', (req,res) => {
 
 app.get('/post', (req, res) => {
     res.send('you are on the post page');
+})
+
+//Connect to Data-Base
+
+mongoose.connect('mongodb+srv://user314:31415>@cluster0-ichxa.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, () => {
+    console.log('connected to the database');
+    
 })
 
 app.listen(4000);
